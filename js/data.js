83d81e4f96108a488252c6bc3a19659006afea37
mikeydebug <cs24b1031@iiitr.ac.in>
2025-04-27 03:30:34 +0530

    terminal

---
+++ js/data.js
@@ -0,0 +1,35 @@
+var data = {
+  name : [
+    {name: 'Name', value: 'Mayank soni'}
+  ],
+  bio: [
+    {name: 'Name', value: 'Mayank Soni'},
+    {name: 'Email', value: 'cs24b1031@iiitr.ac.in'},
+    {name: 'Phone', value: '7496066192'},
+    {name: 'Country', value: 'INDIA'},
+    {name: 'City', value: 'raichur'}
+  ],
+  github: [
+    {name: 'GitHub', value: 'mikeybebug'}
+  ],
+  projects: [
+    {name: 'Bingo box', value: 'https://github.com/mikeydebug/BINGO-BOX'},
+
+    {name: 'portfolio', value: 'https://github.com/mikeydebug/portfolio'},
+
+    {name: 'Color Guessing Game', value: 'https://github.com/mikeydebug/Color-Guessing-Game'},
+
+    {name: 'GPA Calculator', value: 'https://github.com/mikeydebug/GPA-calculator'},
+
+  ],
+  resume: 'assets/Mayank Resume-2.pdf',
+  socials: [
+    {name: 'LinkedIn', value: 'https://www.linkedin.com/in/mayank-soni-2580a9337//'},
+
+    {name: 'GitHub', value: 'https://github.com/mikeydebug'},
+
+    {name: 'Instagram', value: 'https://instagram/bloom.mayank'},
+    
+    {name: 'Website', value: 'https://mayanksoni.me'}
+  ]
+}
